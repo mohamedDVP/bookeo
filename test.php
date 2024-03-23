8281be2340dddee8ace5431e9b19ed131ba1f2e2
mohamedDVP <mohamed.mak@laposte.net>
2024-03-23 11:53:59 +0100

    notions d'héritage et d'interface

--- test.php
+++ test.php
@@ -1,43 +1,27 @@
 <?php
 
-class Product {
-    protected string $name;
-    protected string $description;
-    protected float $price;
-
-    
-
-    public function getName() {
-        return $this->name;
-    }
-
-    public function getDescription() {
-        return $this->description;
-    }
-
-    public function getPrice() {
-        return $this->price;
-    }
-
-    public function setName(string $name) {
-        $this->name = $name;
-    }
-
-    public function setDescription(string $description) {
-        $this->description = $description;
-    }
-
-    public function setPrice(float $price) {
-        $this->price = $price;
-    }
-
-    public function displayProduct() {
-        echo "Name: $this->name, Description: $this->description, Price: $this->price";
-    }
-
-
-
+interface IVehicule {
+    public function Accélérer();
+    public function Freiner();
 }
 
-$product1 = new Product();
-$product1->getDescription();
+class Vehicule implements IVehicule {
+    protected string $marque;
+    protected float $vitesse_max;
+
+    public function Accélérer() {
+        echo "Le véhicule accélère";
+    }
+
+    public function Freiner() {
+        echo "Le véhicule freine";
+    }
+}
+
+class Voiture extends Vehicule {
+    protected int $nombre_roues;
+}
+
+class Bateau extends Vehicule {
+    protected int $nombre_cabines;
+}
